e4991345968c4a3549d60031e9e62b02435c4f96
KimSia Sim <kimcity@gmail.com>
2018-05-29 16:37:38 +0800

    downgrade DRF to 3.7.* to accommodate DJA

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -283,5 +283,27 @@ INSTALLED_APPS += ['compressor']
 STATICFILES_FINDERS += ['compressor.finders.CompressorFinder']
 
 {%- endif %}
+
+# django-rest-framework-json-api
+# ----------------------------------------------------------------------------
+# https://github.com/django-json-api/django-rest-framework-json-api#settings
+REST_FRAMEWORK = {
+  'PAGE_SIZE': 10,
+  'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+  'DEFAULT_PAGINATION_CLASS':
+  'rest_framework_json_api.pagination.PageNumberPagination',
+  'DEFAULT_PARSER_CLASSES': (
+  'rest_framework_json_api.parsers.JSONParser',
+  'rest_framework.parsers.FormParser',
+  'rest_framework.parsers.MultiPartParser'
+  ),
+  'DEFAULT_RENDERER_CLASSES': (
+  'rest_framework_json_api.renderers.JSONRenderer',
+  'rest_framework.renderers.BrowsableAPIRenderer',
+  ),
+  'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
+}
+{% endif %}
+
 # Your stuff...
 # ------------------------------------------------------------------------------
